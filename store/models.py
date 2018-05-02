592cfb024979e96bed514029a7c0305d15fdffce
grail <wohilas@gmail.com>
2018-05-02 15:13:12 +0300

    working celery async call

--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
 import uuid
+from warehouse.sync_order import sync_order
 
 class Order(models.Model):
     id = models.CharField(max_length=128, null=False, unique=True, primary_key=True, default=uuid.uuid1)
@@ -8,7 +10,16 @@ class Order(models.Model):
     comment = models.TextField(null=True)
     created_at = models.DateTimeField(null=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=128, default="some status")
 
 
     def __str__(self):
-        return f'{self.id}: {self.created_at}'
+        return f'{self.id}: {self.status}'
+
+def sync_sign(sender, instance, created, *args, **kwargs):
+    # make rpc call
+    print(sender, instance, created)
+    di = {"id": instance.id}
+    sync_order.delay(di)
+
+post_save.connect(sync_sign, sender=Order)
--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Order(models.Model):
+    id = models.CharField(max_length=128, null=False, unique=True, primary_key=True, default=uuid.uuid1)
+    amount = models.IntegerField(null=False)
+    price = models.FloatField(null=False)
+    comment = models.TextField(null=True)
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=128, default="some status")
+
+
+    def __str__(self):
+        return f'{self.id}: {self.status}'
