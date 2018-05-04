7fe31ae140d9c99b4f9f7b961c0aa50126fbf57a
grail <wohilas@gmail.com>
2018-05-04 12:39:53 +0300

    creates order in warehouse

--- warehouse/tasks.py
+++ warehouse/tasks.py
@@ -5,6 +5,7 @@ from urllib import request as req
 from datetime import datetime
 import time
 from warehouse.models import Order
+import json
 
 @shared_task
 def sync_order(order):
@@ -12,10 +13,14 @@ def sync_order(order):
     time.sleep(2)    
 
     existing_order = Order.objects.filter(id=order['id'])
-    if not existing_order:
+    if not existing_order or existing_order.updated_at < order['updated_at']:  # if None or newer
         resp = req.urlopen(f"http://0.0.0.0:8000/store/orders/{order['id']}").read().decode("utf-8")
         print("got response:", resp)
-        new_order = Order(id=order['id'], amount=order['amount'], price=order['price'],
-                    comment=order['comment'], created_at=datetime.fromtimestamp(order['created_at']),
-                    updated_at=datetime.fromtimestamp(order['updated_at']))
+        print("rsp type:", type(resp))
+        print(len(resp))
+        resp = json.loads(resp)
+        new_order = Order(id=resp['id'], amount=resp['amount'], price=resp['price'],
+                    comment=resp['comment'], created_at=datetime.strptime(resp['created_at'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                    updated_at=datetime.strptime(resp['updated_at'], "%Y-%m-%dT%H:%M:%S.%fZ"))
         new_order.save()
+        
